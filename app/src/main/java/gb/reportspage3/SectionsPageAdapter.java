c120666bc25311becb0121e7648a93c6feca86a8
Gonçalo Barros <goncalo@Goncalos-MacBook-Pro.local>
2018-04-06 17:43:02 +0100

    Initial commit

---
+++ app/src/main/java/gb/reportspage3/SectionsPageAdapter.java
@@ -0,0 +1,27 @@
+package gb.reportspage3;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by goncalo on 06/04/2018.
+ */
+
+public class SectionsPageAdapter extends FragmentPagerAdapter {
+
+
+    public SectionsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/gb/reportspage3/Tab5Fragment.java
@@ -0,0 +1,37 @@
+package gb.reportspage3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by goncalo on 06/04/2018.
+ */
+
+public class Tab5Fragment extends Fragment {
+    private static final String TAG = "Tab5Fragment";
+
+
+    private Button btnTest;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab5_fragment, container, false);
+        btnTest = (Button) view.findViewById(R.id.btnTEST);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
